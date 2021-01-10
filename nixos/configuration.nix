45c6073e6f9edfac91cf673ffe6bdf30e146c68f
Joseph Tan <66340795+dghosef@users.noreply.github.com>
2021-01-10 04:27:35 +0000

    Added home directory to home user

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -88,6 +88,7 @@
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.dghosef = {
     isNormalUser = true;
+    home = "/home/dghosef";
     extraGroups = [ "wheel" # Enable ‘sudo’ for the user.
                     "scanner" "lp"
                      "audio"]; 
