154e90efc0003804c66d960ba1317b860ffd2c31
dghosef <jdtan638@gmail.com>
2021-05-20 03:06:48 -0700

    Added mac config file

---
+++ nixos/mac12.nix
@@ -0,0 +1,17 @@
+{ config, pkgs, lib, ... }:
+{
+  # CHANGE wlan0 TO WHATEVER ELSE IT SHOULD BE(wlo1) on other systems
+  networking.interfaces.wlo1.useDHCP = true;
+  # Disable shutdown in power key
+  services.logind.extraConfig = ''HandlePowerKey=suspend'';
+  services.logind.lidSwitch = "ignore";
+  # Enable bluetooth(really spotty right now)
+  # Switch to headphones by default
+  boot.extraModprobeConfig = ''
+    alias snd-card-0 snd-hda-intel
+    alias sound-slot-0 snd-hda-intel
+    options snd-hda-intel model=dell-m4-1 enable_msi=1
+'';
+  # Use nvidia drivers
+  services.xserver.videoDrivers = [ "nvidia" ];
+}
